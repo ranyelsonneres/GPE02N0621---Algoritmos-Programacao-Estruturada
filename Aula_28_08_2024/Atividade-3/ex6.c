5a9477d28a82de26c822bb8184a6f260eb4ef6df
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2024-08-28 21:00:47 -0300

    Create ex6.c

---
+++ Aula_28_08_2024/Atividade-3/ex6.c
@@ -0,0 +1,15 @@
+//Ex.6
+
+#include <stdio.h>
+
+
+int main(){
+	int num;
+
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+
+    printf("O quadrado desse numero é %d", num * num);
+    
+	return 0;
+}
