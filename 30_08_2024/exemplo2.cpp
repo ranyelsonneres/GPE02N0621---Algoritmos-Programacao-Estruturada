5a941a9353919f98c35c9f2e766d63eac7859676
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2024-08-30 21:39:45 -0300

    Add files via upload

---
+++ 30_08_2024/exemplo2.cpp
@@ -0,0 +1,23 @@
+//Exemplo 2
+//Condicional
+
+#include <stdio.h>
+
+int main(){
+	
+	int num1, num2, num3, comp;
+	
+	num1 = 10;
+	num2 = 12;
+	num3 = 14;
+	
+	if ((num1 > num2) || (num2 < num3)) {
+		printf("X");
+	}else {
+		printf("Y");
+	}
+	
+	
+	
+	return 0;
+}
